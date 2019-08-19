44afd5b785d8cdcbf4bb04d1e9dddea577fc543e
kathemantunez <kathe_mantunez@unitec.edu>
2019-08-18 21:40:58 -0600

    Updates

---
+++ precache-manifest.1b0a61060aa6af2b7b922f31d8bad5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7cff5da7786260339803d062f1fb84",
+    "url": "/Examen_UX_AppRegistro/index.html"
+  },
+  {
+    "revision": "cd579047659d6c9f6344",
+    "url": "/Examen_UX_AppRegistro/static/css/main.5e25fc17.chunk.css"
+  },
+  {
+    "revision": "016cd58d5c4daad234e5",
+    "url": "/Examen_UX_AppRegistro/static/js/2.72f5b94d.chunk.js"
+  },
+  {
+    "revision": "cd579047659d6c9f6344",
+    "url": "/Examen_UX_AppRegistro/static/js/main.f62e6f7f.chunk.js"
+  },
+  {
+    "revision": "11bd0c5bc03bc0be6a21",
+    "url": "/Examen_UX_AppRegistro/static/js/runtime~main.3c9f15ab.js"
+  }
+]);
